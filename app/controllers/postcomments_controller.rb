5065c5ce44fcbabf1027c009155300218f635466
Prelang Builder <builder@prelang.com>
2016-01-08 22:05:28 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/postcomments_controller.rb
+++ app/controllers/postcomments_controller.rb
@@ -1,4 +1,8 @@
 class PostcommentsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_postcomment, only: [:show, :edit, :update, :destroy]
 
   # GET /postcomments
