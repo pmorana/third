693b6c9850bd3c84b3a8637517dcad94465bf65c
Prelang Builder <builder@prelang.com>
2016-01-08 16:18:29 -0600

    Executed 'rake db:migrate' (development and test) to migrate all generated migrations which created schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108220418) do
+ActiveRecord::Schema.define(version: 20160108220448) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -46,6 +46,64 @@ ActiveRecord::Schema.define(version: 20160108220418) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
+  create_table "alerts", force: true do |t|
+    t.datetime "alert_set"
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.string   "alert_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "alerts", ["event_id"], name: "index_alerts_on_event_id"
+  add_index "alerts", ["user_id"], name: "index_alerts_on_user_id"
+
+  create_table "chats", force: true do |t|
+    t.integer  "event_id"
+    t.integer  "user_id"
+    t.text     "body"
+    t.string   "created_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "chats", ["event_id"], name: "index_chats_on_event_id"
+  add_index "chats", ["user_id"], name: "index_chats_on_user_id"
+
+  create_table "events", force: true do |t|
+    t.string   "event_title"
+    t.text     "event_info"
+    t.datetime "event_time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "postcomments", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "event_id"
+    t.integer  "user_id"
+    t.string   "created_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "postcomments", ["event_id"], name: "index_postcomments_on_event_id"
+  add_index "postcomments", ["user_id"], name: "index_postcomments_on_user_id"
+
+  create_table "precomments", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "event_id"
+    t.integer  "user_id"
+    t.string   "created_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "precomments", ["event_id"], name: "index_precomments_on_event_id"
+  add_index "precomments", ["user_id"], name: "index_precomments_on_user_id"
+
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -60,10 +118,26 @@ ActiveRecord::Schema.define(version: 20160108220418) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["username"], name: "index_users_on_username", unique: true
+
+  create_table "votes", force: true do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
 
 end
