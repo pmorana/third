526c9a6a27bfcc2170d8ca7a984efbd13f8bd383
Prelang Builder <builder@prelang.com>
2016-01-08 22:05:32 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Postcomment' to allow for create, update

--- app/admin/postcomment.rb
+++ app/admin/postcomment.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Postcomment do
+  permit_params :title, :body, :created_by
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
